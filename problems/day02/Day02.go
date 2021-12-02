7967bbd6732cdcc827507eee206a457972abba41
Robin Larsson <gwaerondor@gmail.com>
2021-12-02 13:23:27 +0800

    Add solution to day 2

--- problems/day02/Day02.go
+++ problems/day02/Day02.go
@@ -1,22 +1,101 @@
 package main
 
 import (
-	//"aoc/lib"
+	"aoc/lib"
 	"fmt"
+	"strconv"
+	"strings"
+)
+
+type sub struct {
+	X     int
+	Depth int
+}
+
+type aimedSub struct {
+	Sub sub
+	Aim int
+}
+
+type instruction struct {
+	Direction Direction
+	Steps     int
+}
+
+type Direction int
+
+const (
+	Forward Direction = iota
+	Down
+	Up
 )
 
 func main() {
-	in := true
+	in := lib.LinesOfFileOfDay(2)
 	part1 := Day02Part1(in)
-	fmt.Printf("Day 02 part 1: %v\n", part1)
+	fmt.Printf("Day 02 part 1: %d\n", part1)
 	part2 := Day02Part2(in)
-	fmt.Printf("Day 02 part 2: %v\n", part2)
+	fmt.Printf("Day 02 part 2: %d\n", part2)
 }
 
-func Day02Part1(res bool) bool {
+func Day02Part1(lines []string) int {
+	sub := &sub{}
+	instructions := parseInstructions(lines)
+	for _, instruction := range instructions {
+		sub.execute(instruction)
+	}
+	return sub.Depth * sub.X
+}
+
+func Day02Part2(lines []string) int {
+	aimedSub := &aimedSub{}
+	instructions := parseInstructions(lines)
+	for _, instruction := range instructions {
+		aimedSub.execute(instruction)
+	}
+	return aimedSub.Sub.Depth * aimedSub.Sub.X
+}
+
+func (sub *sub) execute(instruction instruction) {
+	switch instruction.Direction {
+	case Up:
+		sub.Depth -= instruction.Steps
+	case Down:
+		sub.Depth += instruction.Steps
+	case Forward:
+		sub.X += instruction.Steps
+	}
+}
+
+func (sub *aimedSub) execute(instruction instruction) {
+	switch instruction.Direction {
+	case Up:
+		sub.Aim -= instruction.Steps
+	case Down:
+		sub.Aim += instruction.Steps
+	case Forward:
+		sub.Sub.X += instruction.Steps
+		sub.Sub.Depth += sub.Aim * instruction.Steps
+	}
+}
+
+func parseInstructions(lines []string) []instruction {
+	res := make([]instruction, len(lines))
+	for i, ln := range lines {
+		parts := strings.Split(ln, " ")
+		direction := parseDirection(parts[0])
+		steps, _ := strconv.Atoi(parts[1])
+		res[i] = instruction{direction, steps}
+	}
 	return res
 }
 
-func Day02Part2(res bool) bool {
-	return res
+func parseDirection(dir string) Direction {
+	switch dir {
+	case "up":
+		return Up
+	case "down":
+		return Down
+	}
+	return Forward
 }
--- problems/day02/Day02_test.go
+++ problems/day02/Day02_test.go
@@ -4,19 +4,26 @@ import (
 	"testing"
 )
 
+var testData []string = []string{
+	"forward 5",
+	"down 5",
+	"forward 8",
+	"up 3",
+	"down 8",
+	"forward 2",
+}
+
 func TestDay02Part1(t *testing.T) {
-	in := false
-	res := Day02Part1(in)
-	exp := true
+	res := Day02Part1(testData)
+	exp := 150
 	if res != exp {
 		t.Fatalf("Expected %v but got %v", exp, res)
 	}
 }
 
 func TestDay02Part2(t *testing.T) {
-	in := false
-	res := Day02Part2(in)
-	exp := true
+	res := Day02Part2(testData)
+	exp := 900
 	if res != exp {
 		t.Fatalf("Expected %v but got %v", exp, res)
 	}
