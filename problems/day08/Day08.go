46214e788f67ba8d74b5cd66505101ebabee968b
Robin Larsson <gwaerondor@gmail.com>
2021-12-08 13:27:31 +0800

    Prepare a bit for day 08 part 2

--- problems/day08/Day08.go
+++ problems/day08/Day08.go
@@ -43,3 +43,16 @@ func in(e int, ls []int) bool {
 	}
 	return false
 }
+
+func solve(input string) map[int]rune {
+	res := map[int]rune{
+		1: 'a',
+		2: 'b',
+		3: 'c',
+		4: 'd',
+		5: 'e',
+		6: 'f',
+		7: 'g',
+	}
+	return res
+}
--- problems/day08/Day08_test.go
+++ problems/day08/Day08_test.go
@@ -25,10 +25,38 @@ func TestDay08Part1(t *testing.T) {
 	}
 }
 
+func TestSolve(t *testing.T) {
+	//   1111     dddd
+	//  2    3   e    a
+	//  2    3   e    a
+	//   4444     ffff
+	//  5    6   g    b
+	//  5    6   g    b
+	//   7777     cccc
+	input := "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+	expected := map[int]rune{1: 'd', 2: 'e', 3: 'a', 4: 'f', 5: 'g', 6: 'b', 7: 'c'}
+	actual := solve(input)
+	if !mapEq(expected, actual) {
+		t.Fatalf("Expected %d, got %d", expected, actual)
+	}
+}
+
 func TestDay08Part2(t *testing.T) {
 	exp := 61229
 	actual := Day08Part1(testData)
 	if exp != actual {
 		t.Fatalf("Expected %d, got %d", exp, actual)
 	}
+}
+
+func mapEq(a, b map[int]rune) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for ak, av := range a {
+		if bv, ok := b[ak]; !ok || bv != av {
+			return false
+		}
+	}
+	return true
 }
